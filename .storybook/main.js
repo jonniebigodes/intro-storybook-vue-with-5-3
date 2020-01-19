1ba568eeaaee346c2f58631712ed475a3f570abf
jonniebigodes <jonniebigodes@hotmail.com>
2020-01-19 18:57:34 +0000

    added knobs registration

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,5 +1,5 @@
 module.exports = {
   // stories: ['../stories/**/*.stories.js'],
   stories: ['../src/components/**/*.stories.js'],
-  addons: ['@storybook/addon-actions', '@storybook/addon-links'],
+  addons: ['@storybook/addon-actions', '@storybook/addon-links', '@storybook/addon-knobs']
 };
